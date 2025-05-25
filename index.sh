4db9c67f364d996bc7e955b78a7afd133fc64c52
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-05-25 13:39:16 +0530

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,12 @@
+curl -X POST https://api.sarvam.ai/v1/chat/completions \
+     -H "Authorization: Authorization" \
+     -H "Content-Type: application/json" \
+     -d '{
+  "messages": [
+    {
+      "role": "system",
+      "content": "content"
+    }
+  ],
+  "model": "sarvam-m"
+}'
